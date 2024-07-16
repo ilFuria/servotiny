1ef2f7a8e1dea2456a7e5f58bbab6efd053d7ffe
Moriambar <ilfuria@gmail.com>
2024-07-16 23:42:19 +0200

    remove useless

--- servo_drv.ino
+++ servo_drv.ino
@@ -96,10 +96,3 @@ void go() {
 
   unsetBusy();
 }
-
-#ifdef __arm__
-// should use uinstd.h to define sbrk but Due causes a conflict
-extern "C" char* sbrk(int incr);
-#else  // __ARM__
-extern char *__brkval;
-#endif  // __arm__
